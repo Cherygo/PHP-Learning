c48eb0130a6da997eebcd6ecf79580dd01f0d333
Cherygo <olexdzhy@gmail.com>
2025-10-25 18:57:03 +0300

    Added functions folder and completed task 1

---
+++ functions/task1.php
@@ -0,0 +1,8 @@
+<?php
+//create a function calculateRectangleArea that returns the area of a rectangle
+function calculateRectangleArea($num1, $num2) : float
+{
+    return $num1 * $num2;
+}
+
+print_r(calculateRectangleArea(2,3));
