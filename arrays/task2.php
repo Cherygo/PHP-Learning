7862f42d76b3aa5a7a4a03e8d9d551135476863e
Cherygo <olexdzhy@gmail.com>
2025-11-03 17:23:07 +0200

    Completed task 2

---
+++ arrays/task2.php
@@ -0,0 +1,10 @@
+<?php
+
+// create a function that takes in an array and returns its first element
+
+function firstElement(array $arr) : string {
+    $element = $arr[0];
+    return $element;
+}
+
+printf(firstElement([1,23,4,5,2,52,6,5,234,324,234,324,13,0]));
