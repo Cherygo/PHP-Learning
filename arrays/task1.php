f6decee8e18fe59f3b43d49f0759f558bc420901
Cherygo <olexdzhy@gmail.com>
2025-11-03 17:20:44 +0200

    Completed task 1

---
+++ arrays/task1.php
@@ -0,0 +1,11 @@
+<?php
+
+//create a fucntion that returns the last element of an array
+
+function lastElement(array $arr) : string 
+{
+    $element = $arr[count($arr) - 1];
+    return $element;
+}
+
+printf(lastElement([1,2,3,4,5,6,7,8,10]));
